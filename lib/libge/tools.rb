f1a8e5d7c7b05216fd839da5654f0bc456a717ca
Alexander Feodorov <vester@unixcomp.org>
2023-04-30 12:10:11 +0400

    Initial commit

---
+++ lib/libge/tools.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tools/version"
+
+module Libge
+  module Tools
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/libge/tools_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Libge::Tools do
+  it "has a version number" do
+    expect(Libge::Tools::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
